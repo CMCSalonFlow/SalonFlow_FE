4dae284ff7937c5705d81ecb76c3b446153dba46
Bùi Ngọc Tuấn <tuan.bui@software.codegym.vn>
2026-08-22 17:43:33 +0700

    chỉnh lại đường dẫn

--- src/core/api/endpoints.js
+++ src/core/api/endpoints.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "";
 
 export const ENDPOINTS = {
     LOGIN: "/api/v1/auth/login",
